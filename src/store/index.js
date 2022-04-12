e9c8c7793c2a0e30298ba9025007a3af902bb45e
tonynnabs <tonynnabs01@gmail.com>
2022-04-12 15:11:10 -0400

    Redux data in components

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,18 @@
 import { createStore } from "redux";
 const counterReducer = (state = {counter: 0}, action) => {
-    return {
-        counter: state.counter + 1
+    if(action.type === "increment"){
+        return {
+            counter: state.counter + 1
+        }
     }
+    if(action.type === "decrement"){
+        return {
+            counter: state.counter - 1
+        }
+    }
+    return state;
 }
 
-const store = redux.createStore(counterReducer);
+const store = createStore(counterReducer);
 
 export default store;
